eb94d68c12225bd8c0f44d62c54462710441ff5e
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-01-20 13:05:48 -0500

    first commit

---
+++ favorite-movies-app/favorite-movies-app/CustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTableViewCell.swift
+//  favorite-movies-app
+//
+//  Created by Kelly Hancox on 1/13/20.
+//  Copyright © 2020 Kelly Hancox. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var movieImageView: UIImageView!
+    @IBOutlet var movieTitle: UILabel!
+    @IBOutlet var movieYear: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
